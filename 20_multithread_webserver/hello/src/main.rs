832e504c94dc543b5d42602e6073abefcceb6473
binn328 <binn328@gmail.com>
2023-08-18 23:44:59 +0900

    add 19

--- 20_multithread_webserver/hello/src/main.rs
+++ 20_multithread_webserver/hello/src/main.rs
@@ -2,9 +2,11 @@
 use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::fs::File;
+
 fn main() {
     // 바인딩을 통해 127.0.0.1:7878로의 수신을 받아들임
-    let listener = TcpListener::bind("localhost:20777").unwrap();
+    let listener = TcpListener::bind("localhost:7878").unwrap();
 
     // incoming은 스트림의 반복자를 반환
     for stream in listener.incoming() {
@@ -16,10 +18,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("hello.html").unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
